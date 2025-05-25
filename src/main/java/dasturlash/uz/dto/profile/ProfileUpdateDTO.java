9894f2e6406c86c71e13021c1d34ce614da3d6f8
Umidjon Makhmudov <umidjonmax0001@gmail.com>
2025-05-25 19:14:47 +0500

    Profile added

--- src/main/java/dasturlash/uz/dto/profile/ProfileUpdateDTO.java
+++ src/main/java/dasturlash/uz/dto/profile/ProfileUpdateDTO.java
@@ -1,16 +1,52 @@
 package dasturlash.uz.dto.profile;
 
+import dasturlash.uz.enums.ProfileRoleEnum;
 import dasturlash.uz.enums.ProfileStatusEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class ProfileUpdateDTO {
-    private Integer id;
+    @NotBlank(message = "Ism bo‘sh bo‘lmasligi kerak")
     private String name;
+    @NotBlank(message = "Familiya bo‘sh bo‘lmasligi kerak")
     private String surname;
+    @NotBlank(message = "Username  bo‘sh bo‘lmasligi kerak")
     private String username;
-    private String password;
-    private ProfileStatusEnum status;
-    private boolean visible;
-    private LocalDateTime createdDate;
+    @NotEmpty(message = "Role bo‘sh bo‘lmasligi kerak")
+    private List<ProfileRoleEnum> roleList;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<ProfileRoleEnum> getRoleList() {
+        return roleList;
+    }
+
+    public void setRoleList(List<ProfileRoleEnum> roleList) {
+        this.roleList = roleList;
+    }
 }
--- src/main/java/dasturlash/uz/repository/ProfileRoleRepository.java
+++ src/main/java/dasturlash/uz/repository/ProfileRoleRepository.java
@@ -1,5 +1,20 @@
 package dasturlash.uz.repository;
 
-public interface ProfileRoleRepository
-{
+import dasturlash.uz.entity.ProfileRoleEntity;
+import dasturlash.uz.enums.ProfileRoleEnum;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ProfileRoleRepository extends CrudRepository<ProfileRoleEntity, Integer> {
+    @Query("select roles from ProfileRoleEntity where profileId =?1")
+    List<ProfileRoleEnum> getRoleListByProfileId(int profileId);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE from ProfileRoleEntity where profileId =?1 and roles =?2")
+    void deleteByIdAndRoleEnum(Integer profileId, ProfileRoleEnum role);
 }
